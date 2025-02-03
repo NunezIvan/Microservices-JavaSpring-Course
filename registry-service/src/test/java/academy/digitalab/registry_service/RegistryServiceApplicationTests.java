0f1c82a6ae14c2f303b43a47ff87cf8ae0355311
NunezIvan <nunezivan124@gmail.com>
2025-02-03 13:25:50 -0500

    Agregando el registry

---
+++ registry-service/src/test/java/academy/digitalab/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitalab.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
