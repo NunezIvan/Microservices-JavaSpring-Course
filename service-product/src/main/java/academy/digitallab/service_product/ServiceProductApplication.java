41c26eed7e4371592c176c6a1b07aaca32655ec0
NunezIvan <nunezivan124@gmail.com>
2025-02-07 13:37:40 -0500

    Adding Actuator

--- service-product/src/main/java/academy/digitallab/service_product/ServiceProductApplication.java
+++ service-product/src/main/java/academy/digitallab/service_product/ServiceProductApplication.java
@@ -3,7 +3,9 @@ package academy.digitallab.service_product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ServiceProductApplication {
 
 	public static void main(String[] args) {
